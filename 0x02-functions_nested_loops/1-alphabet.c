dc3a0509bd018be967bd9c083d7e7191d9be7c71
Scr0vp <hmostaghfir@gmail.com>
2023-06-20 13:41:08 -0700

    prints the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print Alphabet from a to z in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char L;
+		for (L = 'a'; L >= 'z'; L++)
+			_putchar(L);
+
+	_putchar('\n');
+
+}
