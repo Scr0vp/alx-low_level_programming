9f89556c21bc1881bfe79117e13e86c1fac625d5
Scr0vp <hmostaghfir@gmail.com>
2023-06-22 16:01:17 -0700

    draws a straight line in the terminal.

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_line draws a straigh line in the terminal.
+ * print_line - draws a straigh line in the terminal.
  * @n: number of times the character _ should be printed
  */
 
