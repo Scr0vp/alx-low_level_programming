ff4e487ace17b3807bc6fe34f9d68a9084fd50a8
Scr0vp <hmostaghfir@gmail.com>
2023-06-22 11:30:01 -0700

    prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,12 +5,11 @@
  */
 void print_numbers(void)
 {
-    int number;
+	int number;
 
-    for (number = 0; number < 10; number++)
-    {
-        _putchar(number + '0');
-    }
-
-    _putchar('\n');
+	for (number = 0; number < 10; number++)
+	{
+	_putchar(number + '0');
+	}
+	_putchar('\n');
 }
