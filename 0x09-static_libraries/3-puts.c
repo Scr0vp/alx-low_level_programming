a571b060daefbdd391cdddab5617e5fb0ada7df8
Scr0vp <hmostaghfir@gmail.com>
2023-07-07 14:37:57 -0700

    static library libmy.a containing all the functions

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string to the standard output.
+ * @s: The string to be printed.
+ */
+void _puts(char *s)
+{
+	if (s == NULL)
+	{
+	return;
+	}
+	puts(s);
+}
